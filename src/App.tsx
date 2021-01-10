faa84264bf77e4fb6f93934d69d8b5212cd9809c
seiya endo <seiya.endo0830@gmail.com>
2021-01-10 16:45:30 +0900

    implement component using multiple state

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,33 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = (props: defaultProps) => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-  const reset = () => setCount(0)
-  const double = () => setCount(previousCount => previousCount * 2)
-  const devide3 = () => setCount((previousCount): number =>
-    previousCount % 3 === 0 ? previousCount / 3 : previousCount
-  )
+  const reset = () => {
+    setName(props.name)
+    setPrice(props.price)
+  }
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={devide3}>3の倍数のときだけ3で割る</button>
-      </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price => price + 1)}>+1</button>
+      <button onClick={() => setPrice(price => price - 1)}>+1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} type="text"/>
     </>
   )
+}
+
+type defaultProps = {
+  name: string
+  price: number
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
