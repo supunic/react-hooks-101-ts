83d328f73745cfe4816d720fd28cc85a17ed2853
seiya endo <seiya.endo0830@gmail.com>
2021-01-11 14:28:19 +0900

    solve prop dolliing issue

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import AppContext from '../contexts/AppContext'
 
 import { DELETE_EVENT } from '../actions'
 
-const Event = ({ dispatch, event }: any) => {
+const Event = ({ event }: any) => {
+  const { dispatch } = useContext(AppContext)
   const id = event.id
   const handleClickDeleteButton = () => {
     const result = window.confirm(`イベント（id=${id}）を本当に削除しても良いですか？`)
